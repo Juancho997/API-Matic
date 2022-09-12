739bfa8eadc11b2797da27b57c6ad08fc061f132
Juan Bonino <tomasboni9@gmail.com>
2022-09-11 22:47:53 -0300

    first commit

---
+++ plopfile.js
@@ -0,0 +1,85 @@
+export default function (
+    /** @type {import('plop').NodePlopAPI} */ plop) {
+    // create your generators here
+    plop.setGenerator('basics', {
+        description: 'API generator',
+        prompts: [
+            {
+                type: "input",
+                name: "parentModelName",
+                message: "Name your parent DB model"
+            },
+            {
+                type: "input",
+                name: "parentPluralName",
+                message: "Type your parent's DB model name, in plural"
+            },
+            {
+                type: "input",
+                name: "childModelName",
+                message: "Name your child DB model"
+            },            
+            {
+                type: "input",
+                name: "childPluralName",
+                message: "Type your child's DB model name, in plural"
+            },
+        ],
+        actions: [
+            // models
+            {
+                type: "add",
+                path: "src/models/{{snake_case parentModelName}}.model.js",
+                templateFile: "templates/models/parentDbModel.template.hbs"
+            },
+            {
+                type: "add",
+                path: "src/models/{{snake_case childModelName}}.model.js",
+                templateFile: "templates/models/childDbModel.template.hbs"
+            },
+            // routers
+            {
+                type: "add",
+                path: "src/routes/index.routes.js",
+                templateFile: "templates/routes/indexRoutes.template.hbs"
+            },
+            {
+                type: "add",
+                path: "src/routes/{{snake_case parentPluralName}}.routes.js",
+                templateFile: "templates/routes/parentRoutes.template.hbs"
+            },
+            {
+                type: "add",
+                path: "src/routes/{{snake_case childPluralName}}.routes.js",
+                templateFile: "templates/routes/childRoutes.template.hbs"
+            },
+            // routes controllers
+            {
+                type : "add",
+                path: "src/controllers/routes/{{parentPluralName}}.controllers.js",
+                templateFile : "templates/routes/controllers/parentRoutesControllers.template.hbs"
+            },
+            {
+                type : "add",
+                path: "src/controllers/routes/{{childPluralName}}.controllers.js",
+                templateFile : "templates/routes/controllers/childRoutesControllers.template.hbs"
+            },
+
+        ]
+    });
+
+    // plop.setHelper("TitleCase", (str) => {
+    //     return str.replace(/\w\S*/g, function (txt) {
+    //         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    //     });
+    // });
+
+    plop.setHelper("snake_case", (str) => {
+        return str
+            .replace(/\W+/g, " ")
+            .split(/ |\B(?=[A-Z])/)
+            .map((word) => word.toLowerCase())
+            .join("_");
+    });
+
+};
